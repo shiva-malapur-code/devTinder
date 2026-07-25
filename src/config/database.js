17bca41c1fb4aaed1065200b3fef4fac982892f4
Shivashankar Malapur <s.malapur08@gmail.com>
2026-07-25 21:57:40 +0530

    fixed the issue

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://shiva_node:zUUEcuM3JjgUH1R6@shivanode.uquyiff.mongodb.net/devTinder",
+    "mongodb+srv://shiva_node:@shivanode.uquyiff.mongodb.net/devTinder",
   );
 };
 
